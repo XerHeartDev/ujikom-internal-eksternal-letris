b4746aec8851419024c72639cf98ef10ca862388
XerHeartID <teolala.theodore@gmai.com>
2026-08-24 13:48:01 +0700

    fix: Navbar import case sensitivity for Linux (Vercel)

--- app-frontend/web-bookstore/src/layouts/MainLayout.jsx
+++ app-frontend/web-bookstore/src/layouts/MainLayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import NavBar from "../components/NavBar";
 
 export default function MainLayout() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
-      <Navbar />
+      <NavBar />
 
       <main className="grow w-full px-4 sm:px-6 lg:px-8 py-6">
         <Outlet />
